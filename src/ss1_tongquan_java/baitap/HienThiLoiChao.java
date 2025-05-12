3775e91c61f2189a60547fe1e5130a7102087bda
unknown <vanducduy111000222@gmail.com>
2025-05-12 19:25:03 +0700

    UpdateBaiTap

--- src/ss1_tongquan_java/baitap/HienThiLoiChao.java
+++ src/ss1_tongquan_java/baitap/HienThiLoiChao.java
@@ -1,7 +1,12 @@
 package ss1_tongquan_java.baitap;
 
+import java.util.Scanner;
+
 public class HienThiLoiChao {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
     }
 }
