47ca7c76e3928064d94da90b92fb178de2a3a954
unknown <vanducduy111000222@gmail.com>
2025-05-21 18:35:07 +0700

    UpdateBaiTap3.4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import ss4_OOP.StopWatch;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter hours: ");
+        int hours = Integer.parseInt(sc.nextLine());
+        System.out.printf("Enter minutes: ");
+        int minutes = Integer.parseInt(sc.nextLine());
+        System.out.printf("Enter seconds: ");
+        int seconds = Integer.parseInt(sc.nextLine());
+        StopWatch watch =new StopWatch(hours, minutes, seconds);
+        watch.startCount();
     }
+
 }
